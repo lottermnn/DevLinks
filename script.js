121c5c5563d2df128b2f7cf18c3b0037aa20e3e7
Eduardo <eduardolottermann520@gmail.com>
2023-02-16 12:57:29 -0300

    Eduardo

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag imag
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Eduardo Lottermann usando mascara")
+  } else {
+    //se tiver sem light mode, mandar a imagem nomarl
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Sem mascara")
+  }
+}
